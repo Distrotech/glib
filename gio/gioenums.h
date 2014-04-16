34978e4b2f88d9cb6adb4448022643850a3081dd
Dan Winship <danw@gnome.org>
2014-04-16 10:39:12 -0400

    gio: add network connectivity state to GNetworkMonitor
    Add a property to GNetworkMonitor indicating the level of network
    connectivity: none/local, limited, stuck behind a portal, or full.
    
    The default implementation just returns none or full depending on the
    value of is-available.
    
    FIXME bug number

--- gio/gioenums.h
+++ gio/gioenums.h
@@ -1783,6 +1783,31 @@ typedef enum {
   G_SUBPROCESS_FLAGS_INHERIT_FDS           = (1u << 7)
 } GSubprocessFlags;
 
+/**
+ * GNetworkConnectivity:
+ * @G_NETWORK_CONNECTIVITY_LOCAL: The host is not configured with a
+ *   route to the Internet; it may or may not be connected to a local
+ *   network.
+ * @G_NETWORK_CONNECTIVITY_LIMITED: The host is connected to a network, but
+ *   does not appear to be able to reach the full Internet, perhaps
+ *   due to upstream network problems.
+ * @G_NETWORK_CONNECTIVITY_PORTAL: The host is behind a captive portal and
+ *   cannot reach the full Internet.
+ * @G_NETWORK_CONNECTIVITY_FULL: The host is connected to a network, and
+ *   appears to be able to reach the full Internet.
+ *
+ * The host's network connectivity state, as reported by #GNetworkMonitor.
+ *
+ * Since: 2.42
+ */
+typedef enum {
+  G_NETWORK_CONNECTIVITY_LOCAL,
+  G_NETWORK_CONNECTIVITY_LIMITED,
+  G_NETWORK_CONNECTIVITY_PORTAL,
+  G_NETWORK_CONNECTIVITY_FULL
+} GNetworkConnectivity;
+
+
 G_END_DECLS
 
 #endif /* __GIO_ENUMS_H__ */
